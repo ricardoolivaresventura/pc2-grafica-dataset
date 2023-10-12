0547f057a505b00b066e390f6b85640aed27ac57
ricardo leonardo olivares ventura <rlov@MacBook-Air-de-ricardo.local>
2023-10-12 16:01:01 -0500

    Con fe

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import tempfile
 import os
-from flask import Flask, request, redirect, send_file
-from skimage import io
+from flask import Flask, request, redirect, send_file, render_template, jsonify
+from skimage import io, img_as_ubyte
+from skimage.transform import resize
 import base64
 import glob
 import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
+
+# model = load_model('model.h5')
 
 main_html = """
 <html>
@@ -111,20 +115,119 @@ main_html = """
 
 """
 
+predictions_html = """
+    <html>
+<head></head>
+<script>
+  var mousePressed = false;
+  var lastX, lastY;
+  var ctx;
+
+   function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+   }
+
+  function InitThis() {
+      ctx = document.getElementById('myCanvas2').getContext("2d");
+
+
+      numero = getRndInteger(0, 10);
+
+      $('#myCanvas2').mousedown(function (e) {
+          mousePressed = true;
+          Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+      });
+
+      $('#myCanvas2').mousemove(function (e) {
+          if (mousePressed) {
+              Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+          }
+      });
+
+      $('#myCanvas2').mouseup(function (e) {
+          mousePressed = false;
+      });
+  	    $('#myCanvas2').mouseleave(function (e) {
+          mousePressed = false;
+      });
+  }
+
+  function Draw(x, y, isDown) {
+      if (isDown) {
+          ctx.beginPath();
+          ctx.strokeStyle = 'black';
+          ctx.lineWidth = 11;
+          ctx.lineJoin = "round";
+          ctx.moveTo(lastX, lastY);
+          ctx.lineTo(x, y);
+          ctx.closePath();
+          ctx.stroke();
+      }
+      lastX = x; lastY = y;
+  }
+
+  function clearArea() {
+      // Use the identity matrix while clearing the canvas
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  //https://www.askingbox.com/tutorial/send-html5-canvas-as-image-to-server
+  function prepareImg() {
+     var canvas = document.getElementById('myCanvas2');
+     document.getElementById('myImage2').value = canvas.toDataURL();
+  }
+
+
+
+</script>
+<body onload="InitThis();">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" ></script>
+    <div align="center">
+        <h1 id="title">PC2 - COMPUTACIÓN GRÁFICA</h1>
+        <p style="text-align:center, font-size:20px">Ricardo Olivares Ventura - 20192002A</p>
+        <p style="text-align:center, font-size:20px">Sergei Calle Cuadros</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <h1 id="subtitle">🚀Prediciendo dígitos🚀</h1>
+        <p></p>
+        <p></p>
+        <h1>Dibuja un número del 0 al 9</h1>
+        <canvas id="myCanvas2" width="200" height="200" style="border:2px solid black"></canvas>
+        <br/>
+        <br/>
+        <button onclick="javascript:clearArea();return false;">Borrar</button>
+    </div>
+    <div align="center">
+      <form method="post" action="upload_2" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
+      <input id="numero_2" name="numero_2" type="hidden" value="">
+      <input id="myImage2" name="myImage2" type="hidden" value="">
+      <input id="bt_upload_2" type="submit" value="Enviar">
+      </form>
+    </div>
+</body>
+</html>
+
+"""
+
 @app.route("/")
 def main():
     return(main_html)
 
+@app.route("/predictions")
+def predictions():
+    return(predictions_html)
+
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
-        # check if the post request has the file part
         img_data = request.form.get('myImage').replace("data:image/png;base64,","")
         aleatorio = request.form.get('numero')
         print(aleatorio)
         with tempfile.NamedTemporaryFile(delete = False, mode = "w+b", suffix='.png', dir=str(aleatorio)) as fh:
             fh.write(base64.b64decode(img_data))
-        #file = request.files['myImage']
         print("Image uploaded")
     except Exception as err:
         print("Error occurred")
@@ -132,13 +235,30 @@ def upload():
 
     return redirect("/", code=302)
 
+""" @app.route('/upload_2', methods=['POST'])
+def upload2():
+    try:
+        img_data = request.form.get('myImage').replace("data:image/png;base64,","")
+        image = np.array(Image.open(io.BytesIO(base64.b64decode(img_data))).convert('L'))
+        image = resize(image, (28, 28))
+        image = image / 255.0
+        model = getModel()
+        prediction = model.predict(np.expand_dims(image, axis=0))
+        predicted_number = np.argmax(prediction)
+
+        # Return prediction
+        return jsonify({'predicted_number': int(predicted_number)})
+
+    except Exception as err:
+        print("Error occurred during image analysis")
+        print(err)
+        # return jsonify({'error': 'An error occurred during image analysis'})
+    return redirect("/", code=302) """
 
 @app.route('/prepare', methods=['GET'])
 def prepare_dataset():
     images = []
     d = [0, 1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9]
-    # d = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    # d = ["I","N","U"]
     digits = []
     for digit in d:
       filelist = glob.glob('{}/*.png'.format(digit))
@@ -156,14 +276,13 @@ def prepare_dataset():
 @app.route('/X.npy', methods=['GET'])
 def download_X():
     return send_file('./X.npy')
+
 @app.route('/y.npy', methods=['GET'])
 def download_y():
     return send_file('./y.npy')
 
 if __name__ == "__main__":
     digits = [0, 1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9]
-    # digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    # digits = ['U', 'N', 'I']
     for d in digits:
         if not os.path.exists(str(d)):
             os.mkdir(str(d))
---
+++ model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.optimizers import SGD
+
+def getModel(): 
+    X_raw = np.load('X.npy')
+    X_raw = X_raw/255.
+    y = np.load('y.npy')
+    X = []
+    size = (28,28)
+    for x in X_raw:
+        X.append(resize(x, size))
+    X = np.array(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42, stratify=y)
+
+    bs = 16
+    lr = 0.0005
+
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(*size, 1)),
+        MaxPooling2D(),
+        Conv2D(64, (3, 3), activation='relu', padding='same'),
+        MaxPooling2D(),
+        Conv2D(128, (3, 3), activation='relu', padding='same'),
+        MaxPooling2D(),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    optimizer1 = SGD(learning_rate=lr)
+    model.compile(optimizer=optimizer1, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    log = model.fit(X_train, y_train, batch_size=bs, epochs=400, validation_data=(X_test, y_test))
+    return model
+
